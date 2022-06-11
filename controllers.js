04a80c8582d21ee65d0601b4aa5132ac569f3093
dricdrk <cdric.hounnou2@gmail.com>
2022-06-11 17:22:08 -0400

    create models users places and our controllers *

--- controllers.js
+++ controllers.js
@@ -1,16 +1,29 @@
 var mysql = require('mysql2');
 
-var con = mysql.createConnection({
-  database: 'parking',
-  host: "localhost",
-  user: "root",
-  password: "SessiHans99#"
-});
+function createUser(user){
+    let name = user.name;
+    let surname = user.surname;
+    let mail = user.mail;
+    let phone = user.phone;
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
-// function create_user(user){
+    con.connect(function(err) {
+        if (err) throw err;
+        var add_user = "INSERT INTO customers (name, surname, mail, phone) VALUES ("+name+", "+surname+", "+mail+", "+phone+")";
+        con.query(add_user, function (err, result) {
+            if (err) throw err;
+            console.log("user create !");
+          });
+      });
+    
+}
+function get_allUser(user){
+    con.connect(function(err) {
+            if (err) throw err;
+        con.query("SELECT * FROM users WHERE id = "+user.id+"'", function (err, result) {
+            if (err) throw err;
+              console.log(result);
+              return result
+            });
+          });
 
-// }
+}
